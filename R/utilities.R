da9bf533070cd72e60013717fb10126680630a05
ensignathletics <ensignathletics@gmail.com>
2020-11-03 08:36:36 -0700

    first load of new cutilz package

---
+++ R/utilities.R
@@ -0,0 +1,16 @@
+#' Random Letter Generator
+#' @param n the number of random letters you want to select
+#' @param l_prob a vector of probabilities for each of the 26 letters (optional), default is 1/26
+#' @param wreplacement if you want multiple letters, logical TRUE/FALSE for sampling with replacement
+#' @import
+#' @return a random letter of probabilities
+#' @export
+#' @examples
+#' randomletter(1) # select 1 random letter
+#' randomletter(2) # select 2 random letters
+
+
+randomLetter <- function (n, l_prob = rep(1/26, 26), wreplacement = FALSE) {
+  iter <- sample(26, n, replace = wreplacement, prob = l_prob)
+  letters[iter]
+}
